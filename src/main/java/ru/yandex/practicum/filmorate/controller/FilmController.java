135237f688e3a26c60f4e5e48e1881bdaa78e0b1
ChernakovaMSPB <115102637+ChernakovaMSPB@users.noreply.github.com>
2023-04-29 02:47:45 +0300

    add-most-populars

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -18,7 +18,7 @@ public class FilmController {
     private final FilmService filmService;
 
     @PostMapping
-    public Film addFilm(@RequestBody @Valid  Film film) {
+    public Film addFilm(@RequestBody @Valid Film film) {
         log.debug("Получен запрос POST /films.\n" + film.toString());
         return filmService.addFilm(film);
     }
@@ -41,6 +41,7 @@ public class FilmController {
         return filmService.getFilmById(filmId);
     }
 
+    @SuppressWarnings("checkstyle:DesignForExtension")
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable int id, @PathVariable int userId) {
         log.debug(String.format("Получен запрос PUT films/%d/like/%d", id, userId));
@@ -54,8 +55,17 @@ public class FilmController {
     }
 
     @GetMapping("/popular")
-    public List<Film> getPopular(@RequestParam(defaultValue = "10") int count) {
+    public List<Film> getPopular(@RequestParam(defaultValue = "10") int count,
+                                 @RequestParam(required = false) Integer genreId,
+                                 @RequestParam(defaultValue = "0") int year) {
         log.debug(String.format("Получен запрос GET films/popular на %d фильмов", count));
-        return filmService.getPopular(count);
+        return filmService.getPopular(count, genreId, year);
+
+    }
+
+    @GetMapping("/director/{directorId}")
+    public List<Film> getFilmsByDirector(@PathVariable int directorId, @RequestParam String sortBy) {
+        log.debug(String.format("Получен запрос GET /films/directors/%d", directorId));
+        return filmService.getFilmsByDirector(directorId, sortBy);
     }
 }
